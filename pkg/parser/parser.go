e0f934b177d003b86e6675f40b68ccd07d58d45d
Lucas Miguel <lucasvieira.dev@gmail.com>
2022-07-08 23:46:13 +0100

    small refactor

---
+++ pkg/parser/parser.go
@@ -0,0 +1,17 @@
+package parser
+
+import (
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+// converts number as string in uint
+func StringToUint(number string) (uint, error) {
+	num, err := strconv.ParseUint(number, 10, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, "invalid number")
+	}
+
+	return uint(num), nil
+}
