ede2754436ff6b834ece958f34520c02abddf87e
Lucas Miguel <lucasvieira.dev@gmail.com>
2022-12-28 17:53:51 +0100

    added missing file

---
+++ pkg/http/server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// Serve an HTTP server (with graceful shutdown)
+// Reference: https://github.com/go-chi/chi/blob/master/_examples/graceful/main.go
+func Serve(port string, router *chi.Mux) {
+	// The HTTP Server
+	server := &http.Server{Addr: fmt.Sprintf("0.0.0.0:%s", port), Handler: router}
+
+	// Server run context
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	// Listen for syscall signals for process to interrupt/quit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		log.Fatal("shuting down...")
+
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	// Run the server
+	log.Printf("listening on port %s", port)
+	err := server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+
+	// Wait for server context to be stopped
+	<-serverCtx.Done()
+}
